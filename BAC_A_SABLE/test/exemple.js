ac7efff603994017bc335dcd618c0a444edfc69d
SIMON BULLADO <s_bullado51@hotmail.com>
2023-01-26 12:01:59 +0100

    algo prompt prenom age nom

---
+++ BAC_A_SABLE/test/exemple.js
@@ -0,0 +1,16 @@
+function abracadabra() {
+	let prenom = prompt("prenom ?");
+	let nom = prompt("nom ?");
+	let age = prompt("age ?");
+
+	alert(
+		"Sapristi ! On ne m'avait pas prévenu que c'était vous, " +
+			prenom +
+			"! Euh... Je veux dire... Monsieur le grand magicien " +
+			nom +
+			" ! Cela fait déjà " +
+			age +
+			" ans que vous faites rayonner notre contrée !"
+	);
+}
+abracadabra();
